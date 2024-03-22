08505aef69f32f5a5c4d7904b38efb4ec08aa528
andrey <singaevskiy2001@mai.ru>
2024-03-22 14:55:54 +0300

    Step 3

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,13 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static Object username;
+    public static void username(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("May I Have yor name?");
+        String userName = scan.nextLine();
+        System.out.println("Hello " + userName);
+    }
+}
